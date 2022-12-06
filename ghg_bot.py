3e61a8cb2f459e9ca9254b360b8da4725a67061e
BSuzinator <brandon.suzelis@gmail.com>
2022-12-05 23:41:41 -0500

    attempt #1

--- ghg_bot.py
+++ ghg_bot.py
@@ -1,5 +1,7 @@
+from aiohttp import ClientSession
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import asyncio
+import asyncpg
 import bec_rcon
 import configparser
 from dateutil.relativedelta import *
@@ -9,15 +11,17 @@ from dateutil.parser import *
 from datetime import *
 import discord
 from discord import *
+from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import CommandNotFound
 from discord.utils import get
 import esix
-#from ghg_functions import *
+from ghg_functions import *
 #from ghg_automatedTasks import *
 from github import Github
 import json
 import logging
+import logging.handlers
 import mysql.connector
 import os
 import pendulum
@@ -29,25 +33,93 @@ import threading
 import time
 import traceback
 import ts3
+from typing import List, Optional
 
 #Read Config for connection info
 config = configparser.ConfigParser()
 config.read('connectionInfo.ini')
 
-intents = discord.Intents.all()
+#MY_GUILD = discord.Object(id=int(config['Discord']['ghgGuildID']))  # replace with your guild id
 
-client = discord.Client(intents=intents)
+class CustomBot(commands.Bot):
+    def __init__(
+        self,
+        *args,
+        initial_extensions: List[str],
+        db_pool: asyncpg.Pool,
+        web_client: ClientSession,
+        testing_guild_id: Optional[int] = None,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.db_pool = db_pool
+        self.web_client = web_client
+        self.testing_guild_id = testing_guild_id
+        self.initial_extensions = initial_extensions
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+    async def setup_hook(self) -> None:
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+        # here, we are loading extensions prior to sync to ensure we are syncing interactions defined in those extensions.
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+        for extension in self.initial_extensions:
+            await self.load_extension(extension)
 
-client.run(config['Discord']['botToken'])
+        # In overriding setup hook,
+        # we can do things that require a bot prior to starting to process events from the websocket.
+        # In this case, we are using this to ensure that once we are connected, we sync for the testing guild.
+        # You should not do this for every guild or for global sync, those should only be synced when changes happen.
+        if self.testing_guild_id:
+            guild = discord.Object(self.testing_guild_id)
+            # We'll copy in the global commands to test with:
+            self.tree.copy_global_to(guild=guild)
+            # followed by syncing to the testing guild.
+            await self.tree.sync(guild=guild)
+
+        # This would also be a good place to connect to our database and
+        # load anything that should be in memory prior to handling events.
+
+
+async def main():
+
+    # When taking over how the bot process is run, you become responsible for a few additional things.
+
+    # 1. logging
+
+    # for this example, we're going to set up a rotating file logger.
+    # for more info on setting up logging,
+    # see https://discordpy.readthedocs.io/en/latest/logging.html and https://docs.python.org/3/howto/logging.html
+
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.INFO)
+
+    handler = logging.handlers.RotatingFileHandler(
+        filename='discord.log',
+        encoding='utf-8',
+        maxBytes=32 * 1024 * 1024,  # 32 MiB
+        backupCount=10,  # Rotate through 5 files
+    )
+    dt_fmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    # Alternatively, you could use:
+    # discord.utils.setup_logging(handler=handler, root=False)
+
+    # One of the reasons to take over more of the process though
+    # is to ensure use with other libraries or tools which also require their own cleanup.
+
+    # Here we have a web client and a database pool, both of which do cleanup at exit.
+    # We also have our bot, which depends on both of these.
+
+    async with ClientSession() as our_client, asyncpg.create_pool(user='postgres', command_timeout=30) as pool:
+        # 2. We become responsible for starting the bot.
+
+        exts = ['','']
+        async with CustomBot(commands.when_mentioned, db_pool=pool, web_client=our_client, initial_extensions=exts) as bot:
+
+            await bot.start(os.getenv(config['Discord']['botToken'], ''))
+
+
+# For most use cases, after defining what needs to run, we can just tell asyncio to run it:
+asyncio.run(main())
---
+++ ghg_functions.py
@@ -0,0 +1,353 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import asyncio
+import bec_rcon
+import configparser
+from dateutil.relativedelta import *
+from dateutil.easter import *
+from dateutil.rrule import *
+from dateutil.parser import *
+from datetime import *
+import discord
+from discord.ext import commands
+from discord.ext.commands import CommandNotFound
+from discord.utils import get
+import esix
+from github import Github
+import json
+import logging
+import mysql.connector
+import os
+import pendulum
+import random
+import requests
+import schedule
+import subprocess
+import threading
+import time
+import traceback
+import ts3
+
+#Read Config for secrets
+config = configparser.ConfigParser()
+config.read('connectionInfo.ini')
+
+def checkForOP(dateDB=None):
+    print("DateDB: {}".format(dateDB))
+    if dateDB == None:
+        dateDB = pendulum.now().next(pendulum.SATURDAY).strftime('%Y-%m-%d')
+    print("DateDB After check: {}".format(dateDB))
+    mydb = mydbConnect()
+    mycursor = mydb.cursor()
+    checkOPOnDateSQL = "Select opDate FROM ghg_a3.ops WHERE opDate = '{}'".format(dateDB)
+    mycursor.execute(checkOPOnDateSQL)
+    checkOpOnDateR = mycursor.fetchone()
+    print("sql result: {}".format(checkOpOnDateR))
+    mydb.close()
+    isOp = False
+    if checkOpOnDateR:
+        isOp = True
+        print("There is an op this saturday")
+    else:
+        isOp = False
+        print("There is NOT an op this saturday")
+    return isOp
+
+def getPost():
+    postList = []
+    for p in esix.post.search("rating:s order:random score:>=50 -type:webm -type:swf -type:mp4 -type:zip", limit=5):
+        postList.append(p)
+    selectedPost = random.choice(postList)
+    return selectedPost
+
+def getPostNSFW():
+    postListNSFW = []
+    for p in esix.post.search("-rating:s order:random score:>=50 -type:webm -type:swf -type:mp4 -type:zip", limit=5):
+        postListNSFW.append(p)
+    selectedPostNSFW = random.choice(postListNSFW)
+    return selectedPostNSFW
+
+def gitRepo():
+    g = Github(config['Github']['githubToken'])
+    repo = g.get_repo("BSuzinator/GHG_Framework")
+    return repo
+    
+def mydbConnect():
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="webServer",
+    password="w3bS3rv3r#",
+    database="ghg_a3"
+    )
+    return mydb
+
+def rconConnect():
+    rcon = bec_rcon.ARC("127.0.0.1", config['Arma']['serverCommandPass'], 2300)
+    rcon.authorize()
+    return rcon
+
+async def update(self,ctx):
+    subprocess.run("taskkill /f /im arma3server_x64.exe")
+    keyPath = "S:\GHG_A3\A3Files\A3Keys.bat"
+    modPath = "S:\GHG_A3\A3Files\A3ModsUpdate.bat"
+    mainPath = "S:\GHG_A3\A3Files\A3Update.bat"
+    modSuccess = subprocess.call(modPath, shell=True)
+    mainSuccess = subprocess.call(mainPath, shell=True)
+    subprocess.run("taskkill /f /im arma3server_x64.exe")
+    if (os.path.isdir("S:\GHG_A3\A3Master\mpmissions")):
+        subprocess.run(["rmdir", "S:\GHG_A3\A3Master\mpmissions"], shell=True)
+    subprocess.run("mklink /j S:\GHG_A3\A3Master\mpmissions S:\GHG_A3\A3Missions\Training", shell=True)
+    waitUntil(modSuccess + mainSuccess == 0, subprocess.call(keyPath, shell=True),os.startfile (r"E:\Desktop\GHG_Training.lnk"))
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d_%H-%M-%S")
+    serverControlLogChannel = self.bot.get_channel(891142089449029683)
+    await serverControlLogChannel.send("Server Updated and restarted to Training Configuration by {} at {}".format(ctx.author.name,dt_string))
+    #a3UpdateIP = False
+
+def waitUntil(condition, output, output2):
+    wU = True
+    while wU == True:
+        if condition:
+            output
+            output2
+            wU = False
+        time.sleep(5)
+
+async def fnc_updateDatabaseRoles(self,member=None,isAll="no"):
+    guild = self.bot.get_guild(176386774124331008)
+    loaChannelID = 891141241113301002
+    loaChannel = self.bot.get_channel(loaChannelID)
+    activityCheckChannelID = 891141579895607336
+    activityCheckChannel = self.bot.get_channel(activityCheckChannelID)
+    role_id = 891142215848570891
+    loaRole = get(guild.roles, id=role_id)
+    degenerateRole_ID = 925569335328657479
+    degenerateRole = get(guild.roles, id=degenerateRole_ID)
+    activeRole_ID = 446959255262855168
+    activeRole = get(guild.roles, id=activeRole_ID)
+    zeusRole_ID = 711351892969783346
+    zeusRole = get(guild.roles, id=zeusRole_ID)
+    juniorOfficerRole_ID = 888633649656897576
+    juniorOfficerRole = get(guild.roles, id=juniorOfficerRole_ID)
+    officerRole_ID = 728552205283754065
+    officerRole = get(guild.roles, id=officerRole_ID)
+    headAdminRole_ID = 176412413736779777
+    headAdminRole = get(guild.roles, id=headAdminRole_ID)
+    unregisteredList = "Unregistered Users: \n"
+    noTeamspeakIDList = "No Teamspeak ID Users: \n"
+    allMembers = []
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d_%H-%M-%S")
+    if isAll == "yes":
+        allMembers = guild.members
+       # unregisteredList = "{} Unregistered Members:".format(dt_string)
+    else:
+        allMembers.append(member)
+    mydb = mydbConnect()
+    mycursor = mydb.cursor()
+    for member in allMembers:
+        checkUserSQL = "Select discordID FROM users WHERE discordID = {}".format(member.id)
+        mycursor.execute(checkUserSQL)
+        dbID = mycursor.fetchone()
+        if dbID == None:
+            
+            print('{} is unregistered'.format(member.name))
+            if isAll == "yes":
+                unregisteredList = unregisteredList + "{}\n".format(member.name)
+            #if activeRole in member.roles:
+                #emb=discord.Embed(title="GHG Registration", description="Hi, {}! We noticed you have not yet registered into our database. We use this to keep thing fluid and simple for everyone between the Arma Server, Discord, and Teampseak. Please take a moment to do so at your earliest convenience by first joining the Teamspeak server, then in the #bot-commands channel using `$register [steam64ID] [teamspeakUID]`".format(member.name))
+                #try:
+                    #await member.send(embed=emb)
+                    #print("{} | Register Notify Success".format(member.name))
+                #except Exception as e:
+                    #print("{} | Register Notify Fail: {}".format(member.name, e))
+        else:
+            checkNameSQL = "Select discordName FROM users WHERE discordID = {}".format(member.id)
+            mycursor.execute(checkNameSQL)
+            dbname = mycursor.fetchone()
+            if dbname != member.display_name:
+                nameUpdate = "UPDATE users SET discordName = '{}' WHERE discordID = {}".format(member.display_name,member.id)
+                mycursor.execute(nameUpdate)
+                mydb.commit()
+            if headAdminRole in member.roles:
+                headAdminUpdate = 'UPDATE users SET isAdmin = "1" WHERE discordID = {}'.format(member.id)
+                mycursor.execute(headAdminUpdate)
+                mydb.commit()
+            else:
+                headAdminUpdate = 'UPDATE users SET isAdmin = "0" WHERE discordID = {}'.format(member.id)
+                mycursor.execute(headAdminUpdate)
+                mydb.commit()
+            if officerRole in member.roles:
+                officerUpdate = 'UPDATE users SET isOfficer = "1" WHERE discordID = {}'.format(member.id)
+                mycursor.execute(officerUpdate)
+                mydb.commit()
+            else:
+                officerUpdate = 'UPDATE users SET isOfficer = "0" WHERE discordID = {}'.format(member.id)
+                mycursor.execute(officerUpdate)
+                mydb.commit()
+            if juniorOfficerRole in member.roles:
+                juniorOfficerUpdate = 'UPDATE users SET isJuniorOfficer = "1" WHERE discordID = {}'.format(member.id)
+                mycursor.execute(juniorOfficerUpdate)
+                mydb.commit()
+            else:
+                juniorOfficerUpdate = 'UPDATE users SET isJuniorOfficer = "0" WHERE discordID = {}'.format(member.id)
+                mycursor.execute(juniorOfficerUpdate)
+                mydb.commit()
+            if zeusRole in member.roles:
+                zeusUpdate = 'UPDATE users SET isZeus = "1" WHERE discordID = {}'.format(member.id)
+                mycursor.execute(zeusUpdate)
+                mydb.commit()
+            else:
+                zeusUpdate = 'UPDATE users SET isZeus = "0" WHERE discordID = {}'.format(member.id)
+                mycursor.execute(zeusUpdate)
+                mydb.commit()
+            if activeRole in member.roles:
+                activeUpdate = 'UPDATE users SET isActive = "1" WHERE discordID = {}'.format(member.id)
+                mycursor.execute(activeUpdate)
+                mydb.commit()
+            else:
+                activeUpdate = 'UPDATE users SET isActive = "0" WHERE discordID = {}'.format(member.id)
+                mycursor.execute(activeUpdate)
+                mydb.commit()
+            if degenerateRole in member.roles:
+                degenerateUpdate = 'UPDATE users SET isDegenerate = "1" WHERE discordID = {}'.format(member.id)
+                mycursor.execute(degenerateUpdate)
+                mydb.commit()
+            else:
+                degenerateUpdate = 'UPDATE users SET isDegenerate = "0" WHERE discordID = {}'.format(member.id)
+                mycursor.execute(degenerateUpdate)
+                mydb.commit()
+            tsInfoMsg = fnc_updateTSDB(self,member,mydb)
+            if isAll == "yes":
+                noTeamspeakIDList = noTeamspeakIDList + tsInfoMsg            
+            
+            loaDataSQL = "SELECT loaStartDate,loaExpectedReturn,loaMessageID FROM ghg_a3.loas WHERE discordID = '{}';".format(member.id)
+            mycursor.execute(loaDataSQL)
+            loaData = mycursor.fetchone()
+            
+            if loaData:
+                expectedReturnDBObj = loaData[1]
+                loaStartDBObj = loaData[0]
+                loaMessageID = loaData[2]
+                if (expectedReturnDBObj <= now.date()):
+                    loaMessage = await loaChannel.fetch_message(loaMessageID)
+                    expectedReturnDB = expectedReturnDBObj.strftime("%Y-%m-%d")
+                    expectedReturnSTR = expectedReturnDBObj.strftime("%B %d, %Y")
+                    loaStartDB = loaStartDBObj.strftime("%Y-%m-%d")
+                    loaStartSTR = loaStartDBObj.strftime("%B %d, %Y")
+                    embed=discord.Embed(title="LOA for {}".format(member.name), color=0xffffff)
+                    embed.add_field(name="Start Date", value="{}".format(loaStartSTR), inline=False)
+                    embed.add_field(name="Expected Return Date", value="{}".format(expectedReturnSTR), inline=False)
+                    embed.add_field(name="LOA EXPIRED", value="{} should be back now.".format(member.name), inline=False)
+                    await loaMessage.edit(embed=embed)
+                    await member.remove_roles(loaRole, reason="LOA Expired")
+                    loaDeleteSQL = "DELETE FROM ghg_a3.loas WHERE discordID = '{}';".format(member.id)
+                    mydb.commit()
+            
+    mydb.close()
+    if isAll == "yes":
+        await activityCheckChannel.send("Database Update for: {}\n\n{}\n\n{}".format(dt_string,unregisteredList,noTeamspeakIDList))
+
+def fnc_updateTSDB(self,member,mydb):
+    mycursor = mydb.cursor()
+    getRolesSQL = "Select teamspeakUID,isAdmin,isOfficer,isJuniorOfficer,isZeus,isActive,isDegenerate,discordName FROM users WHERE discordID = {}".format(member.id)
+    mycursor.execute(getRolesSQL)
+    userRoles = mycursor.fetchone()
+    teamspeakUID = userRoles[0]
+    isAdmin = userRoles[1]
+    isOfficer = userRoles[2]
+    isJuniorOfficer = userRoles[3]
+    isZeus = userRoles[4]
+    isActive = userRoles[5]
+    isDegenerate = userRoles[6]
+    discordName = userRoles[7]
+    message = "Unable to connect to teamspeak server."
+    tsErrorMsg = ""
+    with ts3.query.TS3Connection("localhost") as ts3conn:
+        try:
+            ts3conn.login(client_login_name=config['Teamspeak']['sqLogin'], client_login_password=config['Teamspeak']['sqPass'])
+            ts3conn.use(sid="1")
+            cldibfromuidResponse = ts3conn.clientgetdbidfromuid(cluid=teamspeakUID)
+            tsClientID = int(cldibfromuidResponse.parsed[0].get('cldbid'))
+            if isAdmin:
+                try:
+                    ts3conn.servergroupaddclient(sgid=6, cldbid=tsClientID)
+                    print("{} added to Server Admin in Teamspeak".format(member.name))
+                except Exception as e:
+                    print("{} already a(n) Server Admin in Teamspeak".format(member.name))
+            else:
+                try:
+                    ts3conn.servergroupdelclient(sgid=6, cldbid=tsClientID)
+                    print("{} removed from Server Admin in Teamspeak".format(member.name))
+                except Exception as e:
+                    print("{} reamains not a(n) Server Admin in Teamspeak".format(member.name))
+            if isOfficer:
+                try:
+                    ts3conn.servergroupaddclient(sgid=24, cldbid=tsClientID)
+                    print("{} added to Officer in Teamspeak".format(member.name))
+                except Exception as e:
+                    print("{} already a(n) Officer in Teamspeak".format(member.name))
+            else:
+                try:
+                    ts3conn.servergroupdelclient(sgid=24, cldbid=tsClientID)
+                    print("{} removed from Officer in Teamspeak".format(member.name))
+                except Exception as e:
+                    print("{} reamains not a(n) Officer in Teamspeak".format(member.name))
+            if isJuniorOfficer:
+                try:
+                    ts3conn.servergroupaddclient(sgid=29, cldbid=tsClientID)
+                    print("{} added to Junior Officer in Teamspeak".format(member.name))
+                except Exception as e:
+                    print("{} already a(n) Junior Officer in Teamspeak".format(member.name))
+            else:
+                try:
+                    ts3conn.servergroupdelclient(sgid=29, cldbid=tsClientID)
+                    print("{} removed from Junior Officer in Teamspeak".format(member.name))
+                except Exception as e:
+                    print("{} reamains not a(n) Junior Officer in Teamspeak".format(member.name))
+            if isZeus:
+                try:
+                    ts3conn.servergroupaddclient(sgid=25, cldbid=tsClientID)
+                    print("{} added to Zeus in Teamspeak".format(member.name))
+                except Exception as e:
+                    print("{} already a(n) Zeus in Teamspeak".format(member.name))
+            else:
+                try:
+                    ts3conn.servergroupdelclient(sgid=25, cldbid=tsClientID)
+                    print("{} removed from Zeus in Teamspeak".format(member.name))
+                except Exception as e:
+                    print("{} reamains not a(n) Zeus in Teamspeak".format(member.name))
+            if isActive:
+                try:
+                    ts3conn.servergroupaddclient(sgid=28, cldbid=tsClientID)
+                    print("{} added to Arma/Active in Teamspeak".format(member.name))
+                except Exception as e:
+                    print("{} already a(n) Arma/Active in Teamspeak".format(member.name))
+            else:
+                try:
+                    ts3conn.servergroupdelclient(sgid=28, cldbid=tsClientID)
+                    print("{} removed from Arma/Active in Teamspeak".format(member.name))
+                except Exception as e:
+                    print("{} reamains not a(n) Arma/Active in Teamspeak".format(member.name))
+            if isDegenerate:
+                try:
+                    ts3conn.servergroupaddclient(sgid=26, cldbid=tsClientID)
+                    print("{} added to Degenerate in Teamspeak".format(member.name))
+                except Exception as e:
+                    print("{} already a(n) Degenerate in Teamspeak".format(member.name))
+            else:
+                try:
+                    ts3conn.servergroupdelclient(sgid=26, cldbid=tsClientID)
+                    print("{} removed from Degenerate in Teamspeak".format(member.name))
+                except Exception as e:
+                    print("{} reamains not a(n) Degenerate in Teamspeak".format(member.name))
+            #clientGroups = ts3conn.servergroupsbyclientid(cldbid=tsClientID)
+            #groupList = "Teamspeak Server Groups for {}:".format(ctx.author.name)
+            #for groupDict in clientGroups:
+                #groupName = groupDict['name']
+                #if groupName != 'Traitor' or groupName != 'Degenerate':
+                    #groupList += "\n{}".format(groupName)
+        except Exception as e:
+            print("{} for {}".format(e,discordName))
+            tsErrorMsg = "{} for {}\n".format(e,discordName)
+    ts3conn.quit()
+    return tsErrorMsg
